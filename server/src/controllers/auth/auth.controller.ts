fe5770a265c50f36e0af9dec2ae7cf80f91b1f8b
Ekong, Emmanuel <53881533+evie-adenoyin@users.noreply.github.com>
2026-03-01 13:08:10 +0100

    Update cookie settings for refresh token

--- server/src/controllers/auth/auth.controller.ts
+++ server/src/controllers/auth/auth.controller.ts
@@ -45,8 +45,8 @@ export const login = async (req: Request, res: Response) => {
     // 5️⃣ Send refresh token as httpOnly cookie (recommended)
     res.cookie('refreshToken', refreshAccessToken, {
       httpOnly: true,
-      secure: process.env['NODE_ENV'] === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
