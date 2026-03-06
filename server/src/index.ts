436438ff99a84c06235784ba753155bdc4bdf707
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2026-03-06 01:02:27 +0100

    config(CORS): uncomment cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env['FRONTEND_URL']!],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan('dev'));
 
