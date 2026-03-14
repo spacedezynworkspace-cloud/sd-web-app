6a04478c09891ab7f640fe50a00fa36712c71222
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2026-03-14 13:57:21 +0100

    fix(app.ts): add app.ts file

---
+++ server/src/app.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './config/db.config';
+import swaggerUI from 'swagger-ui-express';
+import swaggerOpenapiSpecification from './config/swagger.config';
+import morgan from 'morgan';
+
+import authRoutes from './routes/auth.routes';
+import supervisorRoutes from './routes/supervisor/supervisor.routes';
+
+import adminProjectRoutes from './routes/admin/project.routes';
+import adminFinanceRoutes from './routes/admin/finance.routes';
+import adminExpenseRoutes from './routes/admin/expense.routes';
+import adminPaymentRoutes from './routes/admin/payment.routes';
+import adminDashboardRoutes from './routes/admin/dashboard.routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(
+  '/api/v1/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerOpenapiSpecification)
+);
+
+app.use('/api/v1/auth', authRoutes);
+
+app.use('/api/v1/admin/projects', adminProjectRoutes);
+app.use('/api/v1/admin/finances', adminFinanceRoutes);
+app.use('/api/v1/admin/expenses', adminExpenseRoutes);
+app.use('/api/v1/admin/payments', adminPaymentRoutes);
+app.use('/api/v1/admin/dashboard', adminDashboardRoutes);
+
+app.use('/api/v1/supervisors', supervisorRoutes);
+
+let isConnected = false;
+
+async function connect() {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+}
+
+connect();
+
+export default app;
